0e457fbfff04f90ef8ce8be1449df23e2800b409
Prakash Mishra <prakashmishra.niit@gmail.com>
2021-11-17 13:28:19 +0530

    Add New Controller

---
+++ Server/Controllers/MasterController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BSM.Server.Controllers
+{
+    public class MasterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
