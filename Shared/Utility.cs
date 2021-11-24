aa2ef8cc5c8c338d6d3907264fe881ca7b53edf9
Prakash Mishra <prakashmishra.niit@gmail.com>
2021-11-24 10:57:59 +0530

    Add Files

---
+++ Shared/Utility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSM.Shared
+{
+    class Utility
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public int CreatedBy { get; set; }
+        public int UpdatedBy { get; set; }
+    }
+
+    public class CommonResponse
+    {
+        public int status { get; set; }
+        public string message { get; set; }
+        public object responseValue { get; set; }
+    }
+}
