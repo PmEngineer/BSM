f02c600e0c422f49871a9b311cdd3e632464d101
Prakash Mishra <prakashmishra.niit@gmail.com>
2021-11-29 11:23:44 +0530

    Services Commit by Prakash

--- Shared/Model/Masters.cs
+++ Shared/Model/Masters.cs
@@ -8,7 +8,20 @@ namespace BSM.Shared.Model
 {
     public class Masters : Utility
     {
-        public String name { get; set; }
+        public string name { get; set; }
+        public string company_name { get; set; }
+        public string Address { get; set; }
+        public int city_id { get; set; }
+        public int states_id { get; set; }
+        public int country_id { get; set; }
+        public string zip_code { get; set; }
+        public string phone_number { get; set; }
+        public string email { get; set; }
+        public string gst_number { get; set; }
+        public int companyType_id { get; set; }
+       
+        
+        
 
 
     }
