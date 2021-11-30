7864bda34c3a720582f31599c17fbba5e2e4ce2f
Faisal Nafees <faisalsiddiqui5569@outlook.com>
2021-11-30 13:36:33 +0530

    Faisal add parameters in DalMaster

--- Shared/Dal/DalMaster.cs
+++ Shared/Dal/DalMaster.cs
@@ -7,6 +7,7 @@ using System.Data.SqlClient;
 using System.Threading.Tasks;
 using BSM.Shared.Model;
 
+
 namespace BSM.Shared.Dal
 {
   public  class DalMaster
@@ -16,7 +17,18 @@ namespace BSM.Shared.Dal
             SqlCommand cmd = new SqlCommand("proc_master", pobj.connect);
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.Parameters.AddWithValue("@name", pobj.name);
-            
+            cmd.Parameters.AddWithValue("@company_name", pobj.company_name);
+            cmd.Parameters.AddWithValue("@Address", pobj.Address);
+            cmd.Parameters.AddWithValue("@city_id", pobj.city_id);
+            cmd.Parameters.AddWithValue("@states_id", pobj.states_id);
+            cmd.Parameters.AddWithValue("@country_id", pobj.country_id);
+            cmd.Parameters.AddWithValue("@zip_code", pobj.zip_code);
+            cmd.Parameters.AddWithValue("@phone_number", pobj.phone_number);
+            cmd.Parameters.AddWithValue("@email", pobj.email);
+            cmd.Parameters.AddWithValue("@gst_number", pobj.gst_number);
+            cmd.Parameters.AddWithValue("@companyType_id", pobj.companyType_id);
+            cmd.Parameters.AddWithValue("@StartDate", pobj.StartDate);
+            cmd.Parameters.AddWithValue("@EndDate", pobj.EndDate);
             cmd.Parameters.AddWithValue("@CreatedBy", pobj.CreatedBy);
             cmd.Parameters.AddWithValue("@opCode", pobj.OpCode);
             cmd.Parameters.Add("@isException", SqlDbType.Bit);
